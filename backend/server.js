02d765b63ccc08324a50fb52a8e593a2ee6d8d18
Piotr Miszczak <miszpiotr@wp.pl>
2021-01-03 21:56:50 +0100

    Added port to ENV

--- backend/server.js
+++ backend/server.js
@@ -231,4 +231,4 @@ app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 app.use(express.static(path.join(__dirname, '/frontend/build')));
 app.get('*', (req,res)=>res.sendFile(path.join(__dirname, '/frontend/build/index.html')))
 
-app.listen(5000, ()=>console.log('Server started at http://localhost:5000'))
+app.listen(process.env.PORT || 5000, ()=>console.log('Server started at http://localhost:5000'))
